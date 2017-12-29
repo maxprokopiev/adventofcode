018a887de64bcc52da7635040438bc8e2e62221d
Max Prokopiev <me@maxprokopiev.com>
2017-12-29 15:58:35 +0100

    Day 3, part 2

--- src/main/scala/com/adventofcode/edition2015/day3_perfect_houses.scala
+++ src/main/scala/com/adventofcode/edition2015/day3_perfect_houses.scala
@@ -18,22 +18,45 @@ class PerfectHouses {
       new Point(this.x - 1, this.y)
     }
 
+    def moveTo(direction: Char): Point = {
+      direction match {
+        case '>' => this.toEast
+        case '^' => this.toNorth
+        case '<' => this.toWest
+        case 'v' => this.toSouth
+      }    
+    }
+
     override def toString: String = {
       s"($x,$y)"
     }
   }
 
+  private def visitHouse(houses: Map[String, Int], position: Point): Map[String, Int] = {
+    houses + (position.toString -> (houses.getOrElse(position.toString, 0) + 1))
+  }  
+
   def calculateNumberOfHouses(directions: String): Int = {
     val position = new Point(0, 0)
 
     directions.foldLeft((Map(position.toString -> 1), position))( (houses: (Map[String, Int], Point), direction: Char) => {
-      val newPosition = direction match {
-        case '>' => houses._2.toEast
-        case '^' => houses._2.toNorth
-        case '<' => houses._2.toWest
-        case 'v' => houses._2.toSouth
-      }
-      (houses._1 + (newPosition.toString -> (houses._1.getOrElse(newPosition.toString, 0) + 1)), newPosition)
+      val newPosition = houses._2.moveTo(direction)
+      (visitHouse(houses._1, newPosition), newPosition)
     })._1.keySet.size
+  }
+
+  def calculateNumberOfHousesWithRoboSanta(directions: String): Int = {
+    val position = new Point(0, 0)
+
+    directions.grouped(2).foldLeft((Map(position.toString -> 1), position, position))( (houses: (Map[String, Int], Point, Point), directions: String) => {
+      val newSantaPosition = houses._2.moveTo(directions(0))
+      val newRoboSantaPosition = houses._3.moveTo(directions(1))
+
+      (
+       visitHouse(visitHouse(houses._1, newSantaPosition), newRoboSantaPosition),
+       newSantaPosition,
+       newRoboSantaPosition
+      )
+    })._1.keySet.size  
   }
 }
