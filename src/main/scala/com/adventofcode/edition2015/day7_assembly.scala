10a67e633ee1b425d7eab26135e26c622c65363d
Max Prokopiev <me@maxprokopiev.com>
2018-01-16 22:06:50 +0100

    Use default value for the new gate

--- src/main/scala/com/adventofcode/edition2015/day7_assembly.scala
+++ src/main/scala/com/adventofcode/edition2015/day7_assembly.scala
@@ -7,11 +7,25 @@ class Assembly {
   class Gate {
     var name: String = ""
     var signal: Int = -1
-    var operation: String = "SIGNALGATE"
+    var operation: String = ""
     var inputs = ArrayBuffer[Gate]()
   }
 
-  var gates = Map[String, Gate]()
+  def newGate(name: String): Gate = {
+    var gate = new Gate
+    gate.name = name  
+
+    val intRegex = """(\d+)""".r
+    name match {
+      case intRegex(signal) => { gate.signal = signal.toInt }
+      case _ => {}
+    }
+
+    gates(name) = gate
+    gate
+  }
+
+  var gates = Map[String, Gate]().withDefault(newGate)
 
   def applyInstruction(s: String): Unit = {
     val Signal = """(\w+) -> (\w+)""".r
@@ -22,100 +36,29 @@ class Assembly {
       case Signal(arg1, receiver) => {
         println("signal")
 
-        if (!gates.contains(receiver)) {
-          gates(receiver) = new Gate
-          gates(receiver).name = receiver
-        }   
         gates(receiver).operation = "SIGNAL"     
-
-        val intRegex = """(\d+)""".r
-        arg1 match {
-          case intRegex(signal) => {
-            var signalGate = new Gate
-            signalGate.signal = signal.toInt
-            signalGate.name = signal
-            gates(signal) = signalGate
-            gates(receiver).inputs += signalGate          
-          }
-          case _ => {
-            if (!gates.contains(arg1)) {
-              gates(arg1) = new Gate
-              gates(arg1).name = arg1
-            }
-
-            gates(receiver).inputs += gates(arg1)          
-          }
-        }
+        gates(receiver).inputs += gates(arg1)          
       }
 
       case UnaryOp(arg1, receiver) => {
         println("unary op")
 
-        if (!gates.contains(receiver)) {
-          gates(receiver) = new Gate
-          gates(receiver).name = receiver
-        }
-
         gates(receiver).operation = "NOT"
-        
-        if (!gates.contains(arg1)) {
-          gates(arg1) = new Gate
-          gates(arg1).name = arg1
-        }
-
         gates(receiver).inputs += gates(arg1)        
       }
 
       case BinaryOp(arg1, operation, arg2, receiver) => {
         println(s"binary op $operation")
 
-        if (!gates.contains(receiver)) {
-          gates(receiver) = new Gate
-          gates(receiver).name = receiver
-        }
         gates(receiver).operation = operation
-
-        val intRegex = """(\d+)""".r
-        arg1 match {
-          case intRegex(signal) => {
-            var signalGate = new Gate
-            signalGate.signal = signal.toInt
-            signalGate.name = signal
-            gates(signal) = signalGate
-            gates(receiver).inputs += signalGate
-          }
-          case _ => {
-            if (!gates.contains(arg1)) {
-              gates(arg1) = new Gate
-              gates(arg1).name = arg1
-            }
-
-            gates(receiver).inputs += gates(arg1)
-          }
-        }
-
-        arg2 match {
-          case intRegex(signal) => {
-            var signalGate = new Gate
-            signalGate.signal = signal.toInt
-            signalGate.name = signal
-            gates(signal) = signalGate            
-            gates(receiver).inputs += signalGate
-          }
-          case _ => {
-            if (!gates.contains(arg2)) {
-              gates(arg2) = new Gate
-              gates(arg2).name = arg2
-            }          
-            gates(receiver).inputs += gates(arg2)
-          }
-        }        
+        gates(receiver).inputs += gates(arg1)
+        gates(receiver).inputs += gates(arg2)      
       }
     }
   }
 
   def signalOn(s: String): Int = {
-    print(s"calculating signal on $s...")
+    print(s"calculating signal on $s... ")
     
     if (gates(s).signal == -1) {
       println("")
@@ -124,7 +67,7 @@ class Assembly {
         case "SIGNAL" => { gates(s).signal = signalOn(gates(s).inputs(0).name) }
         case "NOT"    => { gates(s).signal = (65535 + ~signalOn(gates(s).inputs(0).name) + 1) % 65535 }
         case "OR"     => { gates(s).signal = signalOn(gates(s).inputs(0).name) | signalOn(gates(s).inputs(1).name) }
-        case "AND"    => { gates(s).signal = signalOn(gates(s).inputs(0).name) & signalOn(gates(s).inputs(1).name)  }
+        case "AND"    => { gates(s).signal = signalOn(gates(s).inputs(0).name) & signalOn(gates(s).inputs(1).name) }
         case "LSHIFT" => { gates(s).signal = signalOn(gates(s).inputs(0).name) << signalOn(gates(s).inputs(1).name) }
         case "RSHIFT" => { gates(s).signal = signalOn(gates(s).inputs(0).name) >> signalOn(gates(s).inputs(1).name) }      
         case _        => { gates(s).signal }
