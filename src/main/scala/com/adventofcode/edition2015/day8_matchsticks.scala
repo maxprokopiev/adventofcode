1873ca900c7d1987d0a46ed6f8cc8ea86f4329b2
Max Prokopiev <me@maxprokopiev.com>
2018-01-17 21:40:34 +0100

    Day 8

--- src/main/scala/com/adventofcode/edition2015/day8_matchsticks.scala
+++ src/main/scala/com/adventofcode/edition2015/day8_matchsticks.scala
@@ -1,43 +1,45 @@
 package com.adventofcode.edition2015
 
 class Matchsticks {
+  def encodeString(s: String): String = {
+    s.foldLeft("")( (result: String, c: Char) => {
+      c match {
+        case '"'  => { result + "\\\"" }
+        case '\\' => { result + "\\\\" }
+        case _    => { result + c }
+      }
+    })
+  }
+
+  class Pair(val count: Int, val context: Symbol)
+
   def inMemoryStringSizeFor(s: String): Int = {
     val hex = Set('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
-    var context = 'start
-    var count = 0
 
-    for (c <- s)
+    s.foldLeft(new Pair(0, 'start))((pair: Pair, c: Char) => {
       c match {
         case elem if hex.contains(elem) => {
-          context match {
-            case 'x' => { context = 'hex1 }
-            case 'hex1 => {
-              context = 'done
-              count += 1
-            }
-            case _ => { count += 1 }
+          pair.context match {
+            case 'x => { new Pair(pair.count, 'hex) }
+            case _  => { new Pair(pair.count + 1, 'done) }
           }
         }
         case '\\' => { 
-          context = 'escape
-        }
-        case '"' => {
-          context match {
-            case 'escape => {
-              context = 'done
-              count += 1
-            }
-            case _ => { count += 1 }
+          pair.context match {
+            case 'esc => { new Pair(pair.count + 1, 'done) }
+            case 'x   => { new Pair(pair.count + 1, 'esc) }
+            case 'hex => { new Pair(pair.count + 3, 'esc) }
+            case _    => { new Pair(pair.count, 'esc) }
           }
         }
         case 'x' => {
-          context = 'hex
+          pair.context match {
+            case 'esc => { new Pair(pair.count, 'x) }
+            case _    => { new Pair(pair.count + 1, 'done) }
+          }
         }
-        case _ => {
-          count += 1
-        }
+        case _ => { new Pair(pair.count + 1, 'done) }
       }
-
-    count
+    }).count
   }
 }
--- src/test/scala/com/adventofcode/edition2015/day8_matchsticks_test.scala
+++ src/test/scala/com/adventofcode/edition2015/day8_matchsticks_test.scala
@@ -9,4 +9,38 @@ class MatchsticksTest extends FlatSpec {
 
     assert(solver.inMemoryStringSizeFor("""z\xc\x03n\"\\h""") == 9)
   }
+
+  it should "calculate the number of characters of code for string literals minus the number of characters in memory for the values of the strings in total for the entire file" in {
+    val solver = new Matchsticks
+
+    var ofCode = 0
+    var inMemory = 0
+
+    val filename = "day8_input.txt"
+    for (line <- Source.fromResource(filename).getLines) {
+      ofCode += line.size
+      inMemory += solver.inMemoryStringSizeFor(line) - 2
+    }    
+    assert((ofCode - inMemory) == 1371)
+  }
+
+  it should "encode string properly" in {
+    val solver = new Matchsticks
+
+    assert(solver.encodeString("\"\"") == "\\\"\\\"")
+  }
+
+  it should "calculate the difference between original and encoded strings" in {
+    val solver = new Matchsticks
+
+    var ofOriginal = 0
+    var ofEncoded = 0
+
+    val filename = "day8_input.txt"
+    for (line <- Source.fromResource(filename).getLines) {
+      ofOriginal += line.size
+      ofEncoded += solver.encodeString(line).size + 2
+    }        
+    assert((ofEncoded - ofOriginal) == 2117)
+  }
 }
